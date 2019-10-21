d373a7be1b13c0bf4872c9d17af3ae8eda102b65
Sherry Tang <sherrytang5379@gmail.com>
2019-10-20 23:56:11 -0500

    This is for Draw Truck hw

--- src/DrawTruck.java
+++ src/DrawTruck.java
@@ -7,19 +7,27 @@ public class DrawTruck extends Application
 {
     @Override
     public void start (Stage myStage) throws Exception {
-        GraphicsContext gc = JGraphics.setUpGraphics(myStage, "Drawing Shapes", 700, 600);
+        GraphicsContext gc = JGraphics.setUpGraphics(myStage, "Truck", 700, 600);
+
+        gc.setFill(Color.BLUE);
+        gc.fillRect(0,0,700,600);
 
         gc.setFill(Color.BLACK);
-        gc.fillRect(50,100,300,150);
+        gc.fillRect(50,300,300,150);
 
         gc.setFill(Color.AQUA);
-        gc.fillRect(270,130,80,50);
+        gc.fillRect(270,340,80,65);
 
         gc.setFill(Color.RED);
-        gc.fillOval(80,215,65,65);
+        gc.fillOval(80,425,65,65);
 
         gc.setFill(Color.RED);
-        gc.fillOval(255,215,65,65);
+        gc.fillOval(255,425,65,65);
+
+        gc.setFill(Color.BLUE);
+        gc.fillOval(380,200,50,50);
+        gc.setStroke(Color.WHITE);
+        gc.strokeOval(380,200,50,50);
 
     }
 }
