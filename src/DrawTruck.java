c95aedb2def9542b785016fdc2828bf2e56bf30a
Sherry Tang <sherrytang5379@gmail.com>
2019-10-21 10:50:34 -0500

    This is for Draw Truck hw

--- src/DrawTruck.java
+++ src/DrawTruck.java
@@ -1,33 +1,50 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.paint.*;
 import java.util.*;
 import javafx.stage.Stage;
 import javafx.scene.canvas.*;
+ import javafx.animation.AnimationTimer.*;
 public class DrawTruck extends Application
 {
+
+
     @Override
     public void start (Stage myStage) throws Exception {
         GraphicsContext gc = JGraphics.setUpGraphics(myStage, "Truck", 700, 600);
+        //to make animation
 
-        gc.setFill(Color.BLUE);
-        gc.fillRect(0,0,700,600);
+        AnimationTimer timer = new AnimationTimer() {
+            double x = 0;
 
-        gc.setFill(Color.BLACK);
-        gc.fillRect(50,300,300,150);
+            @Override
+            public void handle(long now) {
+                gc.setFill(Color.BLUE);
+                gc.fillRect(0, 0, 700, 600);
 
-        gc.setFill(Color.AQUA);
-        gc.fillRect(270,340,80,65);
+                gc.setFill(Color.BLACK);
+                gc.fillRect(50+x, 300, 300, 150);
 
-        gc.setFill(Color.RED);
-        gc.fillOval(80,425,65,65);
+                gc.setFill(Color.AQUA);
+                gc.fillRect(270+x, 340, 80, 65);
 
-        gc.setFill(Color.RED);
-        gc.fillOval(255,425,65,65);
+                gc.setFill(Color.RED);
+                gc.fillOval(80+x, 425, 65, 65);
 
-        gc.setFill(Color.BLUE);
-        gc.fillOval(380,200,50,50);
-        gc.setStroke(Color.WHITE);
-        gc.strokeOval(380,200,50,50);
+                gc.setFill(Color.RED);
+                gc.fillOval(255+x, 425, 65, 65);
 
+                gc.setFill(Color.BLUE);
+                gc.fillOval(380+x, 200, 50, 50);
+                gc.setStroke(Color.WHITE);
+                gc.strokeOval(380+x, 200, 50, 50);
+
+                x+=5;
+
+            }
+        };
+        timer.start();
     }
+
+    public static void main(String [] args) {launch(args);}
 }
